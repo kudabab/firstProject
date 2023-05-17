9beecd98f03d24d9e307b96ce068eee21766562f
Ivan <example@gmail.com>
2023-05-17 23:01:34 +0300

    Use check for correctly ENter

--- script.js
+++ script.js
@@ -16,6 +16,13 @@ const personalMovieDB = {
 for(let i = 0; i < numbersOfFilms; i++){
     const nameMovie = prompt("How the Movie call?");
     const gradeForMovie = prompt("How much do you rate?");
+    console.log(nameMovie.length);
+    console.log(gradeForMovie.length);
+    if(nameMovie.length < 1 || nameMovie.length > 50 || gradeForMovie.length < 1 || gradeForMovie > 10){
+        alert('Not correctly, to write again!');
+        i--;
+    }
+    else
     movie[nameMovie] = gradeForMovie;
 }
 
