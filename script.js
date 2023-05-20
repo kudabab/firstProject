cffad3069b06cd99a9f13bff4e93b7d49a79eee8
Ivan <example@gmail.com>
2023-05-20 21:19:54 +0300

    Use functions and add Created writeYourGenres

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
 "use sctrict";
 
-const numbersOfFilms = prompt("How many films have you already watched?");
+let numbersOfFilms;
+
+function start() {
+    numbersOfFilms = prompt("How many films have you already watched?");
+   
+    while (numbersOfFilms == '' || numbersOfFilms == null || isNaN(numbersOfFilms)){
+        numbersOfFilms = prompt("How many films have you already watched?");
+    }
+}
+
+function rememberMyFilms (){
+    for(let i = 0; i < numbersOfFilms; i++){
+        const nameMovie = prompt("How the Movie call?");
+        const gradeForMovie = prompt("How much do you rate?");
+    
+        if(nameMovie == null || gradeForMovie == null || nameMovie.length < 1 || 
+            nameMovie.length > 50 || gradeForMovie.length < 1 || gradeForMovie > 10 ||
+            isNaN(gradeForMovie)){
+            alert('Not correctly, to write again!');
+            i--;
+        }
+        else
+        movie[nameMovie] = gradeForMovie;
+    }
+}
+
+function detectPersonalLevel() {
+    if(personalMovieDB.count < 10){
+        alert('You watched really few films');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ){
+        alert('You the classic viewer')
+    } else if( personalMovieDB.count > 30){
+        alert('You the Pokemono!')
+    } else 
+        alert('ERROR!');
+}
+
+function writeMyDb () {
+    if(personalMovieDB.privat == false)
+        console.log(personalMovieDB);
+    else
+        alert('Error with access');
+}
+
+function writeYourGenres () {
+    for(let i = 0; i < 3; i++){
+        const answer = prompt("Your favorite genre?");
+        if(answer > 0 && answer < 4)
+        array[i] = answer;
+        else {
+            alert('Not HAVE? again');
+            i--;
+        }
+    }
+}
+
+start();
 let array = [];
 let movie = {};
 let actor = {};
@@ -13,27 +69,11 @@ const personalMovieDB = {
 
 };
 
-for(let i = 0; i < numbersOfFilms; i++){
-    const nameMovie = prompt("How the Movie call?");
-    const gradeForMovie = prompt("How much do you rate?");
+rememberMyFilms();
 
-    if(nameMovie.length < 1 || nameMovie.length > 50 || gradeForMovie.length < 1 || gradeForMovie > 10){
-        alert('Not correctly, to write again!');
-        i--;
-    }
-    else
-    movie[nameMovie] = gradeForMovie;
-}
+detectPersonalLevel();
 
-if(personalMovieDB.count < 10){
-    alert('You watched really few films');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ){
-    alert('You the classic viewer')
-} else if( personalMovieDB.count > 30){
-    alert('You the Pokemono!')
-} else 
-    alert('ERROR!');
+writeYourGenres();
 
-
-console.log(personalMovieDB);
+writeMyDb();
 
